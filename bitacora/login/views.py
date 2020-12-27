9b44f20066c9d6506bfc9b56dace75f3c93deba7
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2020-12-27 12:57:28 -0300

    make login page visible

--- bitacora/login/views.py
+++ bitacora/login/views.py
@@ -1,3 +1,26 @@
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.views import generic
+from django.db import models
+from login.forms import LoginForm
 
 # Create your views here.
+
+class LoginView (generic.FormView):
+    model = User
+    template_name = 'login/index.html'
+    form_class = LoginForm
+    success_url = 'menu'
+    
+    def identificar_user(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+          login(request, user)
+          context = {'user': user}
+          return render(request, 'menu', context)
+        else:
+          return HttpResponse('usuario invalido')
